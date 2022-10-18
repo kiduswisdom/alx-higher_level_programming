2c552050dce5dcd7d6d7f8d9ebe26b9db69de97e
kiduswisdom <kidusjob@gmail.com>
2022-10-18 13:28:11 -0700

    Simple rectangle

--- 0x08-python-more_classes/tests/0-main.py
+++ 0x08-python-more_classes/tests/0-main.py
@@ -4,3 +4,7 @@ Rectangle = __import__('0-rectangle').Rectangle
 my_rectangle = Rectangle()
 print(type(my_rectangle))
 print(my_rectangle.__dict__)
+
+guillaume@ubuntu:~/0x08$ ./0-main.py
+<class '0-rectangle.Rectangle'>
+{}
