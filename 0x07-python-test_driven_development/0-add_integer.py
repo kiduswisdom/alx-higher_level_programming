4073ca3f1ce7a8744bc182934d970b53a0753b87
kiduswisdom <kidusjob@gmail.com>
2022-10-24 22:36:02 -0700

    update-file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 """
-This is the addition module.
-it adds 2 integers
-a and b must be first casted
+Describes a function to adding two integers
 """
 
 
 def add_integer(a, b=98):
-    """a and are integers
-    Returns an integer: the addition of a and b
+    """Take the arguments to add.
+    a: One of the numbers, default not defined.
+    b: The other number, default 98.
+    Return: int(a + b)
     """
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    if not (isinstance(a, int)):
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if not (isinstance(b, int)):
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    return (a + b)
+    return int(a) + int(b)
