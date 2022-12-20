65e01637e7623cc14e633c27c0e90e7a8fd5026b
kiduswisdom <kidusjob@gmail.com>
2022-12-19 21:41:40 -0800

    update

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
+
 exports.converter = function (base) {
-  return function (num) {
-    return (num.toString(base));
+  return function (number) {
+    return number.toString(base);
   };
 };
