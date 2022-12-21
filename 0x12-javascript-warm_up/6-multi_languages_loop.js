5507129eb39a675413320cee03b2899869890ad7
kiduswisdom <kidusjob@gmail.com>
2022-12-20 22:39:14 -0800

    Update-my js code

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const languages = ['C lang is funny', 'Python is cool lang', 'JavaScript is amazing'];
 
-for (let index = 0; index < myArray.length; index++) {
-  const element = myArray[index];
-  console.log(element);
+for (const language of languages) {
+  console.log(language);
 }
