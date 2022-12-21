e95aae675fae0062f67cc9763d5a160ee0ed6aaa
kiduswisdom <kidusjob@gmail.com>
2022-12-21 11:19:26 -0800

    my_safe_filter_states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
-'''
-Take in argyment but make it s safe from MySQL injections.
-'''
+"""
+Script that lists all values in the `states` table of `hbtn_0e_0_usa`
+where `name` matches the argument `state name searched`.
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+    state name searched (str)
+"""
+
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
+    mySQL_u = sys.argv[1]
+    mySQL_p = sys.argv[2]
+    db_name = sys.argv[3]
 
-        conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                               user=argv[1], passwd=argv[2], db=argv[3])
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM states WHERE name=%s ORDER BY states.id",
-                    (argv[4], ))
-        query_rows = cur.fetchall()
-        for row in query_rows:
-                print(row)
-        cur.close()
-        conn.close()
+    searched_name = sys.argv[4]
+
+    # By default, it will connect to localhost:3306
+    db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id",
+                (searched_name, ))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
