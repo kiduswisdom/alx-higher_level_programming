828bb0f889dc75d71a60fa840fd05387b4c3525c
kiduswisdom <kidus Ataklti>
2023-01-16 21:21:20 -0800

    Added-to fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-'''
-Script that fetches https://intranet.hbtn.io/status
-'''
-from urllib import request
+"""fetches https://alx-intranet.hbtn.io/status"""
 
-url = 'https://intranet.hbtn.io/status'
-with request.urlopen(url) as response:
-        the_page = response.read()
-            print('Body response:')
-                print('\t- type: {}'.format(type(the_page)))
-                    print('\t- content: {}'.format(the_page))
-                        print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
+
+if __name__ == "__main__":
+        import urllib.request
+
+            with urllib.request.urlopen('https://alx-intranet.hbtn.io/
+                                            status/') as response:
+                        html = response.read()
+                                print('Body response:')
+                                        print('\t- type: {}'.format(type(html)))
+                                                print('\t- content: {}'.format(html))
+                                                        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
