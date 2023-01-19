2a6bf509370135e391fb6bb5ac21699102a5ff97
kiduswisdom <kidus Ataklti>
2023-01-18 20:46:36 -0800

    fiexed

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""takes in a URL, sends a request to the URL and displays
-the body of the response."""
+""" script that takes in a URL, sends a request to
+ the URL and displays the body of the response."""
+import sys
 
+import requests
 
 if __name__ == "__main__":
-    import requests
-    import sys
-
-    r = requests.get(argv[1])
+    url = sys.argv[1]
+    r = requests.get(url)
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
